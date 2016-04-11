b40b63704d51a5ef237624b4048061c6d9ce82f3
Исамов Шахоб <shisamov@at-consulting.ru>
2016-04-11 15:34:16 +0300

    refactor

--- src/HelloWorld.php
+++ src/HelloWorld.php
@@ -6,16 +6,19 @@ class HelloWorld
      * @var PDO
      */
     private $pdo;
+
     public function __construct(PDO $pdo)
     {
         $this->pdo = $pdo;
     }
+
     public function hello($what = 'World')
     {
         $sql = "INSERT INTO hello VALUES (" . $this->pdo->quote($what) . ")";
         $this->pdo->query($sql);
         return "Hello $what";
     }
+
     public function what()
     {
         $sql = "SELECT what FROM hello";
